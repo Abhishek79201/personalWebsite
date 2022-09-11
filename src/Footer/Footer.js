df4d2fbe1f89df502c400b06f1bc619810d01d4a
Abhishek79201 <vaghelaabhishek2580@gmail.com>
2022-09-11 18:26:32 +0530

    Initial Commit

---
+++ src/Footer/Footer.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './Footer.css';
+function Footer() {
+  return (
+    <div>
+      <div className="footer">
+        <div className="footer-container">
+          <div className="message-for-users">
+            <span>Contact me at Your pleasure</span>
+          </div>
+          <div className="social-links">
+            <div className="social-links-h">Social</div>
+            <div className="social-links-items">
+              <a href="">Instagram</a>
+              <a href="">Twitter</a>
+              <a href="">Github</a>
+              <a href="">LinkedIn</a>
+            </div>
+          </div>
+          <div className="social-links">
+            <div className="social-links-h">Contact</div>
+            <div className="social-links-items">
+              <a href="">Mail</a>
+              <a href="">Whatsapp</a>
+              <a href="">Telegram</a>
+              <a href="">+91 8200-394-360</a>
+            </div>
+          </div>
+          <div className="footer-menu social-links">
+            <div className="footer-menu-h social-links-h">Other Menu</div>
+            <div className="footer-menu-links social-links-items">
+              <p className="footer-menu-links-items">Home.</p>
+              <p className="footer-menu-links-items">About.</p>
+              <p className="footer-menu-links-items">Projects.</p>
+              <p className="footer-menu-links-items">Contact.</p>
+            </div>
+          </div>
+          <div className="footer-btn">
+            <a href="" className="footer-btn-a">
+              Get Started!
+            </a>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <div className="left-footer-bottom">
+            <p>India</p>
+          </div>
+          <div className="right-footer-bottom">
+            <p>2022. Abhishek Vaghela. ALL RIGHT RESERVED</p>
+          </div>
+        </div>
+      </div>
+      <div className="footer-ticker">
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+        <div className="footer-ticker-item">&nbsp; - Abhishek Vaghela </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
